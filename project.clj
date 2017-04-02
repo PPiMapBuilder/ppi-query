7d847613d20fead85c536c63f3bc133c45ca5c75
gcornut <gcornut@gmail.com>
2017-04-02 23:47:52 +0200

    Add AOT maven compilation for PMB interop.

--- project.clj
+++ project.clj
@@ -17,9 +17,18 @@
                  [org.hupo.psi.mi.psicquic/psicquic-simple-client "1.3.3"]
                  [org.hupo.psi.mi.psicquic/psicquic-registry-client "1.1.0"]]
 
+  :pom-plugins [[com.theoryinpractise/clojure-maven-plugin "1.8.1"
+                 ;; this section is optional, values have the same syntax as pom-addition
+                 {:configuration [:sourceDirectories [:sourceDirectory "src"]]
+                  :extensions "true"
+                  :executions ([:execution [:id "compile"]
+                                [:goals ([:goal "compile"])]
+                                [:phase "compile"]])}]]
+
   :plugins [[lein-auto "0.1.3"]]
   :profiles {:dev
-              {:dependencies
+              {:aot :all
+               :dependencies
                 [[proto-repl "0.3.1"]
                  [proto-repl-charts "0.3.2"]
                  [org.clojure/test.check "0.9.0"]]}})
