0763e146a4012625c4a69b818fc986d9ebfe91bb
gcornut <gcornut@gmail.com>
2017-07-28 18:23:26 +0200

    Fix xml node spec

--- src/ppi_query/xml.clj
+++ src/ppi_query/xml.clj
@@ -6,14 +6,14 @@
 (s/def ::tag keyword?)
 (s/def ::attrs (s/nilable map?))
 (s/def ::content (s/nilable (s/coll-of ::document)))
-(s/def ::node (s/keys :req-un [::tag] :opt-un [::attrs ::content]))
+(s/def ::node (s/keys :req-un [::tag ::attrs ::content]))
 (s/def ::document (s/or :text string? :node ::node :nil nil?))
 
 (defmacro node [& {:keys [tag attrs content]}]
   "Creates an xml node spec describind its tag, attibutes and content."
   `(ps/map-spec :tag ~tag
-                :attrs ~(or attrs `nil?)
-                :content ~(or content `nil?)))
+                :attrs ~(or attrs `::attrs)
+                :content ~(or content `::content)))
 
 (defmacro text-node [tag]
   "Spec for xml node containing only text."
