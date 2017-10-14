d113fcd0c2db6ec3127ac3f2e147c830c5dacf49
gcornut <gcornut@gmail.com>
2017-10-14 19:15:05 +0200

    WIP integration of ppi-query interactome in PMB.

--- src/ppi_query/api.clj
+++ src/ppi_query/api.clj
@@ -1,16 +1,38 @@
 (ns ppi-query.api
   (:require [ppi-query.interaction.psicquic.registry :as reg]
+            [ppi-query.network :as network]
             [ppi-query.organism :as org]))
+
+(declare to-java)
+
+(defn map-to-java [object]
+  (->> object
+       (map (fn [[k v]] [(name k) (to-java v)]))
+       (into {})))
+
+(defn seq-to-java [object]
+  (->> object
+       (map (fn [v] (to-java v)))
+       (into (list))))
+
+(defn to-java [object]
+  (cond
+    (map? object) (map-to-java object)
+    (seq? object) (seq-to-java object)
+    :else object))
 
 (defn to-string-map [kw-map]
   "Transform a keyword keyed map into a string keyed map."
   (->> kw-map
-       (map (fn [[k v]] [(name k) v]))
+       (map
+         (fn [[k v]]
+           [(name k) v]))
        (into {})))
 
 (definterface Api
   (^java.util.List getPsicquicServices [])
-  (^java.util.List getOrganisms []))
+  (^java.util.List getOrganisms [])
+  (^java.util.List getInteractome [^java.util.List database-names ^java.lang.Long organism-id]))
 
 (gen-class
   :name ppi_query.api.ApiImpl
@@ -26,3 +48,8 @@
   (->> org/inparanoid-organism-repository
        (map to-string-map)))
 
+(defn api-getInteractome [this database-names organism-id]
+  (let [organism (org/inparanoid-organism-by-id organism-id)]
+    (->>
+      (network/fetch-interactome database-names organism)
+      (to-java))))
