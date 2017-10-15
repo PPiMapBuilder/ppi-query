96fa1515a55dc283669aecc477f0028f511e24fb
gcornut <gcornut@gmail.com>
2017-10-15 14:20:32 +0200

    WIP integration of ppi-query protein network in PMB.

--- src/ppi_query/api.clj
+++ src/ppi_query/api.clj
@@ -1,24 +1,31 @@
 (ns ppi-query.api
   (:require [ppi-query.interaction.psicquic.registry :as reg]
             [ppi-query.network :as network]
+            [ppi-query.protein :as protein]
             [ppi-query.organism :as org]))
 
 (declare to-java)
 
+(defn nil-or-empty? [e]
+  (or (nil? e) (and (seq? e) (empty? e))))
+
 (defn map-to-java [object]
   (->> object
+       #_(remove (comp (nil-or-empty? second)))
        (map (fn [[k v]] [(name k) (to-java v)]))
        (into {})))
 
 (defn seq-to-java [object]
   (->> object
        (map (fn [v] (to-java v)))
+       (remove nil-or-empty?)
        (into (list))))
 
 (defn to-java [object]
   (cond
     (map? object) (map-to-java object)
-    (seq? object) (seq-to-java object)
+    (string? object) object
+    (seqable? object) (seq-to-java (seq object))
     :else object))
 
 (defn to-string-map [kw-map]
@@ -31,8 +38,15 @@
 
 (definterface Api
   (^java.util.List getPsicquicServices [])
+
   (^java.util.List getOrganisms [])
-  (^java.util.List getInteractome [^java.util.List database-names ^java.lang.Long organism-id]))
+
+  (^java.util.List getInteractome
+    [^java.util.List database-names ^java.lang.Long organism-id])
+
+  (^java.util.List getProteinNetwork
+    [^java.util.List databases-names ^java.lang.Long ref-organism-id
+     ^java.util.List protein-ids ^java.util.List other-organism-ids]))
 
 (gen-class
   :name ppi_query.api.ApiImpl
@@ -52,4 +66,15 @@
   (let [organism (org/inparanoid-organism-by-id organism-id)]
     (->>
       (network/fetch-interactome database-names organism)
+      (to-java))))
+
+(defn api-getProteinNetwork [this databases-names ref-organism-id protein-ids other-organism-ids]
+  (let [ref-organism (org/inparanoid-organism-by-id ref-organism-id)
+        other-organisms (map org/inparanoid-organism-by-id other-organism-ids)
+        proteins (map (partial protein/->Protein ref-organism) protein-ids)]
+    (->>
+      (network/fetch-protein-network
+        databases-names ref-organism proteins other-organisms)
+      (second)
+      (map #(dissoc % :original-interactions))
       (to-java))))
